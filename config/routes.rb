cd0d9f87c06282e5594e7b8b77fe50ad28c51a38
colmexdev <github@colmex.mx>
2019-04-09 18:15:33 -0500

    Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  get "/" => "principal#index", :as => :index
+  scope "(:locale)", locale: /(en)|(es)/ do
+    get (locale == "en" ? "calendar" : "calendario") => "principal#calendario", :as => :calendario
+  end
+
+  get "/(:locale)" => "principal#index", :as => :main
   root to: "principal#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
