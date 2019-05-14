bca606db6c25808e5b97a802be45d86509754e32
colmexdev <github@colmex.mx>
2019-05-14 14:30:09 -0500

    Panel

--- app/models/documento.rb
+++ app/models/documento.rb
@@ -1,2 +1,7 @@
 class Documento < ApplicationRecord
+
+  has_attached_file :documento, url: 'assets/documentos/:basename-:id.:extension', path: ':rails_root/public/assets/documentos/:basename-:id.:extension'
+  validates_attachment_presence :documento
+  validates_attachment_content_type :documento, content_type: ['image/jpg', 'image/png', 'image/jpeg']
+
 end
