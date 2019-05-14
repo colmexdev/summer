2039869583d46d23b9855d044b1364cce0763fd9
colmexdev <github@colmex.mx>
2019-05-14 15:08:42 -0500

    Panel

--- app/models/documento.rb
+++ app/models/documento.rb
@@ -2,7 +2,7 @@ class Documento < ApplicationRecord
   before_save :delete_documento, if: -> {documento_del == 1}
   attr_accessor :documento_del
 
-  has_attached_file :documento, url: 'assets/documentos/:id/:basename.:extension', path: ':rails_root/public/assets/documentos/:id/:basename.:extension', default_url: '/vacio.png'
+  has_attached_file :documento, url: '/assets/documentos/:id/:basename.:extension', path: ':rails_root/public/assets/documentos/:id/:basename.:extension', default_url: '/vacio.png'
   validates_attachment_presence :documento
   validates_attachment_content_type :documento, content_type: ['application/pdf', 'image/png']
 
