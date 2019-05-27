d05d05dd05d368d08ced0c64b480904651b223a3
colmexdev <github@colmex.mx>
2019-05-27 14:27:27 -0500

    Cambios

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -1,8 +1,10 @@
-{en:
-  date: {
-    formats: {
-      no_year: lambda { |time, _| "%B #{time.day.ordinalize}" }
-      with_year: lambda { |time, _| "%B #{time.day.ordinalize} %Y" }
+{
+  en: {
+    date: {
+      formats: {
+        no_year: lambda { |time, _| "%B #{time.day.ordinalize}" }
+        with_year: lambda { |time, _| "%B #{time.day.ordinalize} %Y" }
+      }
     }
   }
 }
