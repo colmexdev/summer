b0137b4f33d11c112406840489093dc66888043c
colmexdev <github@colmex.mx>
2019-05-28 11:25:28 -0500

    Cambios

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -2,8 +2,8 @@
   en: {
     date: {
       formats: {
-        no_year_html: lambda { |time, _| "%B #{time.day.ordinalize.gsub(/(\p{L}+)/,'<sup>\1</sup>'}" },
-        with_year_html: lambda { |time, _| "%B #{time.day.ordinalize.gsub(/(\p{L}+)/,'<sup>\1</sup>'} %Y" }
+        no_year_html: lambda { |time, _| "%B #{time.day.ordinalize.gsub(/(\p{L}+)/,'<sup>\1</sup>')}" },
+        with_year_html: lambda { |time, _| "%B #{time.day.ordinalize.gsub(/(\p{L}+)/,'<sup>\1</sup>')} %Y" }
       }
     }
   }
